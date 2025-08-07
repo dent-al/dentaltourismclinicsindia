e317db5e39ec031b014e6c6792cb77d63d4f4f65
Ashwani singh <ashwanisingh22222@gmail.com>
2025-08-07 19:26:17 +0530

    changes

--- backend/Router/UserRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const userRegistration= require("../Controllers/userRegisteration")
-const userLogin = require("../Controllers/userLogin");
-
-const router = express.Router();
-router.post("/Registration",userRegistration);
-router.post("/Login",userLogin);
-
-module.exports=router
