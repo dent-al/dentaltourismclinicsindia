410d3b27e2950562af3e0a71139815e1ad1cb230
Dent-al <urjadentalclinic@gmail.com>
2025-09-06 14:07:51 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-const database=async (req,res) => {
+
+const database = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/dentalclinic')
-      console.log('Database connected successfully');
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('Database connected successfully');
   } catch (error) {
     console.error('Database connection failed:', error);
   }
-}
+};
 
 module.exports = database;
