d063da4f2eadfd9c5dfa947d95af9ca0bcb4f4d3
Dent-al <urjadentalclinic@gmail.com>
2025-09-06 14:52:01 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const database = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(mongodb+srv://job-portal:vivek123@cluster0.2naawoy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0);
     console.log('Database connected successfully');
   } catch (error) {
     console.error('Database connection failed:', error);
